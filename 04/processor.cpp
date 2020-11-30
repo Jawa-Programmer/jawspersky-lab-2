5876a85b8d749e45efd6b1efa315d35dbf0cfd8c
Jawa-Programmer <jawaprogrammer@gmail.com>
2020-11-30 09:38:11 +0300

    увеличена декомпозиция. Теперь некоторые части программы разделены в разные заголовочные файлы, что должно увеличить читаемость кода

--- 04/processor.cpp
+++ 04/processor.cpp
@@ -198,38 +198,7 @@ namespace jpl
 		}
 		return out;
 	}
-	///--- reg_operand ---///
-	std::ostream& reg_operand::print(std::ostream& out) const
-	{
-		adr->print(out << "(");
-		return out;
-	}
 	
-	///--- label_operand ---///
-	
-	label_operand::label_operand(const std::string &lab)
-	{
-		if(!is_valid_name(lab)) throw std::logic_error("incorrect var name");
-		label = lab;
-	}
-	
-	std::ostream& label_operand::print(std::ostream& out) const
-	{
-		out << label;
-		return out;
-	}
-	
-	///--- ram_operand ---///
-	ram_operand::ram_operand(const label_operand &adr_) : adr(adr_.copy()), is_lab(true) {}
-	
-	operand* ram_operand::copy() const {ram_operand *tmp = new ram_operand(*adr); tmp->is_lab = is_lab; return tmp;}
-	
-	std::ostream& ram_operand::print(std::ostream& out) const
-	{
-		if(!is_lab) out << "[";
-		adr->print(out);
-		return out;
-	}
 	
 	///--- controller ---///
 	
@@ -431,7 +400,7 @@ namespace jpl
 		}
 		return out;
 	}
-	
+	///--- processor ---///
 	bool processor::is_free(const operand& op) const
 	{
 		switch(op.type())
